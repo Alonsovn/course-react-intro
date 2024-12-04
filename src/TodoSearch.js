98ff22ede52b9fe491e8b128135aa279d12ab6da
Alonso <alonsonh94@gmail.com>
2024-12-03 23:17:42 -0600

    Add searchValue state

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,21 @@
 import "./TodoSearch.css";
+import React from "react";
 
 function TodoSearch() {
-  return <input placeholder="Learn React" className="TodoSearch" />;
+  const [searchValue, setSearchValue] = React.useState("");
+
+  console.log("busqueda: " + searchValue);
+
+  return (
+    <input
+      placeholder="Learn React"
+      className="TodoSearch"
+      value={searchValue}
+      onChange={(event) => {
+        setSearchValue(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
